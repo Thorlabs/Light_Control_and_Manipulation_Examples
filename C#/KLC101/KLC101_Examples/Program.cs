052731854b85762793996628f2accd50c309851e
Matthew Anzelmi <manzelmi@Thorlabs.com>
2022-04-21 11:26:05 -0400

    KLC example updated

---
+++ C#/KLC101/KLC101_Examples/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLC101_Examples
+{
+    class Program
+    {
+        //Imports for method in C++ based dll
+        [DllImport("KLCCommandLib_x64.dll", EntryPoint = "List")]
+        public static extern int List(out char[] list, int listSize);
+
+        [DllImport("KLCCommandLib_x64.dll", EntryPoint = "Open")]
+        public static extern int Open(char[] deviceString, int baudRate, int timeout);
+
+        [DllImport("KLCCommandLib_x64.dll", EntryPoint = "Close")]
+        public static extern int Close(int deviceHandle);
+
+        [DllImport("KLCCommandLib_x64.dll", EntryPoint = "SetEnable")]
+        public static extern int SetEnable(int deviceHandle, char enableState);
+
+        [DllImport("KLCCommandLib_x64.dll", EntryPoint = "SetVoltage1")]
+        public static extern int SetVoltage1(int deviceHandle, float voltage);
+
+        static void Main(string[] args)
+        {
+            //Check for connected devices. If none are found, close
+            char[] list;
+            int numDevices = List(out list, 2048);
+            Console.WriteLine(numDevices);
+
+            if (numDevices == 0)
+            {
+                Console.WriteLine("No Devices connected");
+                return;
+            }
+
+            //Get a handle to the device
+            int deviceHandle = Open("COM5".ToCharArray(), 9600, 3000);
+
+
+            //Enable the device
+            SetEnable(deviceHandle, '1') ;
+
+            //Set Voltage
+            SetVoltage1(deviceHandle, 5.5f);
+
+            //Close the device
+            Close(deviceHandle);
+
+        }
+    }
+}
